0f17b0f0096ee6eb0e89e96d3169302492731c12
JoanGuinart <guinartcasasjoan@gmail.com>
2025-08-23 13:12:23 +0200

    feat: styart width single page but have error from await params

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -1,26 +1,38 @@
 import Heart from "@/components/Heart";
+import styles from "../../../styles/SingleCharacterPage.module.scss";
 import { fetchMarvelCharacterById } from "@/lib/marvel";
 import Image from "next/image";
 
-export default async function CharacterPage({
-  params,
-}: {
+interface CharacterPageProps {
   params: { id: string };
-}) {
-  const data = await fetchMarvelCharacterById(params.id);
+}
+
+export default async function CharacterPage({ params }: CharacterPageProps) {
+  const { id } = params; 
+  const data = await fetchMarvelCharacterById(id);
   const character = data.data.results[0];
 
   return (
-    <main style={{ padding: "20px" }}>
-      <h1>{character.name}</h1>
-      <Image
-        src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-        alt={character.name}
-        width={300}
-        height={300}
-      />
-      <Heart id={character.id} />
-      <p>{character.description || "No description available."}</p>
+    <main>
+      <section className={styles.singleCharacter}>
+        <Image
+          src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+          alt={character.name}
+          width={300}
+          height={300}
+          priority
+        />
+        <div className={styles.characterInfo}>
+          <div>
+            <h1>{character.name}</h1>
+            <Heart id={character.id} heartClass={styles.heart} width={24} height={22}/>
+          </div>
+          <p>{character.description || "No description available."}</p>
+        </div>
+      </section>
+      <section className={styles.comicsSection}>
+        COMICS
+      </section>
     </main>
   );
 }
